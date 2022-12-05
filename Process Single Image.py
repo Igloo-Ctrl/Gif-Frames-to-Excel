00e31ad52edf193e247540138812b91ecca6264b
ryanjones <ryanleejones@outlook.com>
2022-12-05 19:05:36 +0000

    Before major changes, again.

--- Process Single Image.py
+++ Process Single Image.py
@@ -2,14 +2,18 @@ from PIL import Image
 import os
 from os.path import exists
 import uuid
+from openpyxl import Workbook, load_workbook
 
 # enter the file path to your image here, for example, /Users/johndoe/Downloads/cat.png
-filepath = "D:\Downloads\grinch.jpg"
-
+# filepath = "D:\Downloads\grinch.jpg"
+filepath = "/Users/ryanjones/Downloads/IMG_0929.jpeg"
 
 class Variables:
-    filepath = ""
-    filename = ""
+    image_width = ""
+    image_height = ""
+    image_filepath = ""
+    image_filename = ""
+    excel_file_path = ""
 
 
 def check_for_folder():
@@ -51,19 +55,41 @@ def create_and_move(filepath):
 
 def process_image(filepath):
     open_image = Image.open(filepath)
-    image_width, image_height = open_image.size[0], open_image.size[1]
+    Variables.image_width, Variables.image_height = open_image.size[0], open_image.size[1]
     # rgb_image = open_image.convert("rgb")
 
     data_string = ""
-    for i in range(image_width):
-        for j in range(image_height):
+    for i in range(Variables.image_width):
+        for j in range(Variables.image_height):
             data_string += f"{open_image.getpixel((i, j))[0]}, {open_image.getpixel((i, j))[1]}, " \
                            f"{open_image.getpixel((i, j))[2]}\n"
 
-    with open(f"{Variables.filepath}/rgb_text.txt", "w") as f:
+    with open(f"{Variables.image_filepath}/rgb.txt", "w") as f:
         f.write(data_string)
         print(f"RGB text file created.")
+    create_work_book()
+def create_work_book():
+    """creates a workbook by the name specified above and creates as many sheets as stated above
+    deletes the first sheet and then saves everything"""
+    wb = Workbook()
+    wb.create_sheet(title=f"{Variables.image_filename}")
+    del wb["Sheet"]
+    Variables.excel_file_path = f"{Variables.image_filepath}/excel.xls"
+    wb.save(Variables.excel_file_path)
+    print("Excel Workbook created.")
 
+def input_rbg_into_excel():
+    workbook = load_workbook(Variables.excel_file_path)
+    row, column = 1, 1
+    sheet = workbook.active
+    with open(f"rgb.txt", "r") as f:
+        data = f.readlines()
+        for i in data:
+            if column > Variables.image_width:
+                row += 1
+                column = 1
+            sheet.cell(column=column, row=row, value=i)
+            column += 1
 
 def main():
     check_for_folder()
