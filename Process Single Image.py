e6355e2403aa4e6b60cc60e694a147116be7a3e3
ryanjones <ryanleejones@outlook.com>
2022-12-06 08:48:05 +0000

    It works!

--- Process Single Image.py
+++ Process Single Image.py
@@ -1,12 +1,13 @@
 import time
-from PIL import Image
 import os
 from os.path import exists
 import uuid
+
+from PIL import Image
 from openpyxl import Workbook, load_workbook
 
 # enter the file path to your image here, for example, /Users/johndoe/Downloads/cat.png
-filepath = "/Users/ryanjones/Downloads/the-manager-robert-de-niro.png"
+desired_filepath = "/Users/ryanjones/Downloads/the-manager-robert-de-niro.png"
 
 # time taken
 start = time.process_time()
@@ -22,6 +23,7 @@ class Variables:
 
 
 def check_for_folder():
+    """check for the images folder in the current directory and if it isn't found, creates it"""
     print(f'Checking "{os.getcwd()}" for "images" folder.')
     if exists("images"):
         print('"Images" folder found.')
@@ -32,19 +34,23 @@ def check_for_folder():
 
 
 def validate_filepath():
-    if filepath == "":
+    """checks to see if the filepath is empty, if so exits the program, otherwise proceeds with the process
+    catches an IOError if a valid image doesn't exist at the filepath or if anything below is poorly configured"""
+    if desired_filepath == "":
         exit('Filepath empty, please change the filepath at the top of this script to a valid location.')
     else:
         try:
-            Image.open(filepath)
-            print(f'Valid image found at "{filepath}". Proceeding.')
-            create_and_move(filepath)
-            process_image(filepath)
+            Image.open(desired_filepath)
+            print(f'Valid image found at "{desired_filepath}". Proceeding.')
+            create_and_move(desired_filepath)
+            process_image(desired_filepath)
         except IOError:
             print("Invalid file type.")
 
 
 def create_and_move(filepath):
+    """stores information in the Variables class, creates a unique directory for the process and makes a copy of
+    the original image there"""
     # directory
     Variables.image_filename = os.path.basename(filepath)
     generated_filename = str(uuid.uuid4())
@@ -59,12 +65,14 @@ def create_and_move(filepath):
 
 
 def process_image(filepath):
+    """iterates through the pixels of the image, grabbing their RGB values and adding them to a big string
+    finally, this string is saved in a text document"""
     open_image = Image.open(filepath)
     Variables.image_width, Variables.image_height = open_image.size[0], open_image.size[1]
     # rgb_image = open_image.convert("rgb")
 
     data_string = ""
-    # i = height, j = width
+    # i = height, j = width (??)
     for i in range(Variables.image_height):
         for j in range(Variables.image_width):
             data_string += f"{open_image.getpixel((j, i))[0]}, {open_image.getpixel((j, i))[1]}, " \
@@ -75,8 +83,8 @@ def process_image(filepath):
         print(f"RGB text file created.")
     create_work_book()
 def create_work_book():
-    """creates a workbook by the name specified above and creates as many sheets as stated above
-    deletes the first sheet and then saves everything"""
+    """creates a workbook, deletes the first sheet (typically named Sheet) and creates a new one based off
+    the image name"""
     wb = Workbook()
     wb.create_sheet(title=f"{Variables.image_filename}")
     del wb["Sheet"]
@@ -85,7 +93,9 @@ def create_work_book():
     print("Excel Workbook created.")
     input_rbg_into_excel()
 
+
 def input_rbg_into_excel():
+    """reads the text file and iterates through it, inputting the values into the Excel sheet"""
     workbook = load_workbook(Variables.excel_file_path)
     row, column = 1, 1
     sheet = workbook.active
@@ -102,6 +112,8 @@ def input_rbg_into_excel():
     write_vba_macros()
 
 def write_vba_macros():
+    """writes VBA macros specific to the image used, the first one concerns changing the cell size which is more
+    general, the second is used for colouring the sheet"""
     comment = f"'These macros were created for the image: {Variables.image_filename}. Run the first to resize " \
               f"the cells, run the second to colour them in.\n"
     macro_one = """Sub ChangeColumnWidth()
@@ -127,9 +139,9 @@ Next i
 
 End Sub
     """
-    macros_together = f"{comment}{macro_one}{macro_two}"
+    compiled_text = f"{comment}{macro_one}{macro_two}"
     with open(f"{Variables.image_filepath}/vba_macros.txt", "w") as f:
-        f.write(macros_together)
+        f.write(compiled_text)
     print("Finished writing VBA macros.")
     print(F"All done! Time taken: {time.process_time() - start} seconds.")
 
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Gif-Frames-to-Excel
-A programmatic way of converting the RGB values of each individual pixel in a GIF to data that can be further processed and inserted into an excel document. The process, besides procurring the gif, resizing and splitting is all done automatically, assuming the correct paramaters for your specific case are accounted for.
+A programmatic way of converting the RGB values of each individual pixel in a GIF to data that can be further processed and inserted into an Excel document. The process, besides procuring the gif, resizing and splitting is all done automatically, assuming the correct parameters for your specific case are accounted for.
 
 ![image](https://user-images.githubusercontent.com/107010803/205056554-e9f4eeee-58f0-4c6d-b2fe-12a11525db00.png)
 
-Note: the colouring and formatting of the excel file was done via VBA macros.
+Note: the colouring and formatting of the Excel file was done via VBA macros.
 
 # Steps To Use
 Coming soon?
